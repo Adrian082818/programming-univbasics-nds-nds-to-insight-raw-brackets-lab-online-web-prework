043e8c97b7ae75eafac72c183ddccdabdb64b317
Adrian Mohabir <mohabiradrian@gmail.com>
2019-11-17 19:41:27 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -9,8 +9,8 @@ while row_index < nds.length do
   directors_name = nds[row_index][:name]
   column_index = 0 
   total[directors_name] = 0 
-  while column_index < directors_name.length do 
-   movies = nds[row_index][:movies]
+  movies = nds[row_index][:movies]
+  while column_index < movies.length do 
  directors_gross = movies[column_index][:worldwide_gross]
   total[directors_name] += directors_gross
   return total
